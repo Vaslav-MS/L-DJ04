085c71deaf14d3e0d9b03974613bfb835f34cf4c
Vaslav <2key@rambler.ru>
2025-01-10 15:26:57 +0500

    add fin code

--- djcod25/main/urls.py
+++ djcod25/main/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('new', views.new),
-    path('data', views.data),
-    path('test', views.test)
+    path('', views.index, name='home'),
+    path('about', views.about, name='about')
 ]
--- djcod25/main/views.py
+++ djcod25/main/views.py
@@ -2,14 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'main/index.html')
+    data = {
+        'caption':'Парусная регата'
+    }
+    return render(request, 'main/index.html', data)
 
-def new(request):
-    return render(request, 'main/new.html')
-
-def data(request):
-    return HttpResponse('<h1>Это страница DATA</h1>')
-
-def test(request):
-    return HttpResponse('<h1>Это страница TEST</h1>')
+def about(request):
+    return render(request, 'main/about.html')
 
